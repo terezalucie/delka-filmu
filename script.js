063ee0565e83d485e97c9c0ec948a5e56b60750b
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-02-05 15:39:45 +0100

    prvniUlozeni

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-/* Úkol - Vymyslete, jak použít funkci Math.random a různé zaokrouhlovací funkce probírané v této lekci k simulování hodu klasickou šestistěnnou kostkou. S použitím vhodných funkcí sestavte výraz, který vygeneruje náhodné celé číslo mezi 1 a 6.
+/* Jeden lístek do divadla :i[Pěst na oko] stojí 12 euro. Spočítejte měsíční příjem divadla ze vstupného, přichází-li průměrně 174 návštěvníků na jedno představení a divadlo hraje 15 představení měsíčně. Uložte výsledek do proměnné prijem.
+Hodnotu proměnné prijem vypište do stránky.
+Divadlo se rozhodlo prodávat studentské vstupné ve výši 65 % plného vstupného. Jak se změní měsíční příjem divadla pokud víme, že 40 % návštěvníků jsou studenti?
+Vypište do stránky i příjem divadla se započítanou slevou pro studenty.
+*/
 
-Zamyslete se nad tím, zda vámi vytvořený výraz generuje všechna čísla skutečně se stejnou pravděpodobností. Vemte v úvahu, že funkce Math.random generuje náhodná čísla mezi 0 (včetně) a 1 (vyjma). Je tedy malinká pravěpodobnost, že občas padne přesně číslo 0. Naopak číslo 1 padnout nemůže.
- */
+let cenaListku = 12
+let hraNavstevnici = 174
+let hraMesic = 15
+let prijem = (cenaListku * hraNavstevnici) * hraMesic
 
-
-/* let nahodneCislo = Math.random()
-let vysledekHodu = Math.floor(nahodneCislo * 6) + 1
-
-let vysledek = vysledekHodu
-console.log("Hod kostkou: " + vysledek) */
-
-let nahodneCislo = Math.random()
-let vysledekHodu = Math.floor(nahodneCislo * 6) + 1
-let vysledek = vysledekHodu
-
-console.log("Hod kostkou: " + vysledek)
-document.body.innerHTML = <h1>("Hod kostkou: " + vysledek)</h1>
-document.body.innerHTML = "Hod kostkou: " + vysledek
+document.body.innerHTML = prijem
